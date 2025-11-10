9c1799df148f2e22f6c58e27d7a3092b59c2ec99
pink-clarinet65 <conniemutua@gmail.com>
2025-11-10 12:44:06 +0300

    changes to html and javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("New Generator");
+function generateQuote(event) {
+  event.preventDefault();
+
+  new Typewriter("#quote", {
+    strings: "Creativity takes courage",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
